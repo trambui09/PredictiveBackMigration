27fbdca4ff44be141b9a00a707357e7d2784447a
Philipp Lackner <mail@pl-coding.com>
2024-10-19 12:42:14 +0200

    Implement custom predictive back press animation

--- app/src/main/java/com/plcoding/predictivebackmigration/screens/TextScreen.kt
+++ app/src/main/java/com/plcoding/predictivebackmigration/screens/TextScreen.kt
@@ -1,23 +1,32 @@
 package com.plcoding.predictivebackmigration.screens
 
+import androidx.activity.BackEventCompat
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -33,9 +42,42 @@ fun TextScreen(
     val longText = stringResource(R.string.long_text)
 
     var isTextExpanded by remember { mutableStateOf(false) }
+    var backProgress: Float? by remember {
+        mutableStateOf(null)
+    }
+    var textHeightExpanded by remember {
+        mutableStateOf(0.dp)
+    }
+    var textHeightCollapsed by remember {
+        mutableStateOf(0.dp)
+    }
 
-    BackHandler(isTextExpanded) {
-        isTextExpanded = false
+    val onBackCallback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackProgressed(backEvent: BackEventCompat) {
+                backProgress = backEvent.progress
+            }
+
+            override fun handleOnBackPressed() {
+                isTextExpanded = false
+                backProgress = null
+            }
+
+            override fun handleOnBackCancelled() {
+                backProgress = null
+            }
+        }
+    }
+
+    val backPressedDispatcher = LocalOnBackPressedDispatcherOwner.current!!.onBackPressedDispatcher
+    DisposableEffect(backPressedDispatcher, isTextExpanded) {
+        if(isTextExpanded) {
+            backPressedDispatcher.addCallback(onBackCallback)
+        }
+
+        onDispose {
+            onBackCallback.remove()
+        }
     }
 
     Column(
@@ -55,11 +97,35 @@ fun TextScreen(
             Text(text = "Next")
         }
 
+        val density = LocalDensity.current
         Text(
             text = if (isTextExpanded) longText else shortText,
             style = MaterialTheme.typography.bodyMedium,
             textAlign = TextAlign.Center,
             modifier = Modifier
+                .onGloballyPositioned {
+                    when {
+                        isTextExpanded && backProgress == null -> {
+                            textHeightExpanded = with(density) {
+                                it.size.height.toDp()
+                            }
+                        }
+                        !isTextExpanded -> {
+                            textHeightCollapsed = with(density) {
+                                it.size.height.toDp()
+                            }
+                        }
+                    }
+                }
+                .then(
+                    if(backProgress != null) {
+                        Modifier
+                            .heightIn(min = textHeightCollapsed)
+                            .height(
+                                textHeightExpanded * (1f - (backProgress ?: 0f))
+                            )
+                    } else Modifier
+                )
                 .fillMaxWidth()
                 .clickable { isTextExpanded = !isTextExpanded }
                 .animateContentSize()
